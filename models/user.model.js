0393244bb69028c337953f9849d2b2febc7db78b
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-09-20 14:48:32 +0200

    modified user role to include phone number

--- models/user.model.js
+++ models/user.model.js
@@ -1,34 +1,39 @@
 const { Schema, model } = require("mongoose");
-const validateEmail = require("../utils/validateEmail")
+const validateEmail = require("../utils/validateEmail");
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      unique: true 
-  },
+      unique: true,
+    },
     password: {
       type: String,
       require: true,
       select: false,
     },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: 'Email address is required',
-    validate: [validateEmail, 'Please fill a valid email address'],
-    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
-    required: true,
-  },
-  role: {
+    number: Number,
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+      required: true,
+    },
+    role: {
       type: Schema.Types.String,
       enum: ["super", "moderator", "user", "eateryAccount"],
       default: "user",
-      required: true
+      required: true,
     },
-  },{ timestamps: true },
+  },
+  { timestamps: true }
 );
 
 const User = model("User", userSchema);
