a5cb13a446684e4912bcbe90f3f6d3d04e3d75e8
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-09-20 12:39:45 +0200

    finished eatery application review form

--- routes/moderate.routes/moderate.eateries.routes.js
+++ routes/moderate.routes/moderate.eateries.routes.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
-const Eatery = require("../../models/eatery.model")
+require("dotenv");
+const router = require("express").Router();
+const Eatery = require("../../models/eatery.model");
 const isMod = require("../../middleware/isMod.js");
 
 // mod route to view all eateries despite isReputable Status
@@ -11,59 +12,54 @@ router.get("/", isMod, async (req, res, next) => {
   } catch (error) {
     res.status(400).send(error.message);
   }
-  });
+});
 
-  // mod route to view all pending applications
+// mod route to view all pending applications
 router.get("/pending", isMod, async (req, res, next) => {
   try {
-    const allEateries = await Eatery.find({isReputable: "pending"})
+    const allEateries = await Eatery.find({ isReputable: "pending" });
     res.status(200).json(allEateries);
   } catch (error) {
     res.status(400).send(error.message);
   }
-  });
+});
 
 // mod route to view eatery by ID
-router.get("/:id", isMod, async (req,res,next) => {
+router.get("/:id", isMod, async (req, res, next) => {
   try {
-    const eatery = await Eatery.findById(req.params.id)
+    const eatery = await Eatery.findById(req.params.id);
     res.status(200).json(eatery);
   } catch (error) {
     res.status(400).send(error.message);
   }
-})
+});
 
 // moderator can review application
 //and update isReputable status
-router.patch("/:id", isMod, async (req,res,next) => {
+router.patch("/:id", isMod, async (req, res, next) => {
   try {
-    const {applicationOutcome, moderatorNotes} = req.body
-    console.log(applicationOutcome)
+    const { applicationOutcome, moderatorNotes } = req.body;
+    console.log(applicationOutcome);
     const eatery = await Eatery.findOneAndUpdate(
-      {_id: req.params.id},
-      {isReputable: applicationOutcome,
-        moderatorNotes: moderatorNotes},
-      {new: true})
+      { _id: req.params.id },
+      { isReputable: applicationOutcome, moderatorNotes: moderatorNotes },
+      { new: true }
+    );
     res.status(200).json(eatery);
   } catch (error) {
     res.status(400).send(error.message);
   }
-})
+});
 
 // allows moderator to remove eatery from database
-router.delete("/:id", isMod, async (req,res,next) => {
-  console.log('test')
+router.delete("/:id", isMod, async (req, res, next) => {
+  console.log("test");
   try {
-    const eatery = await Eatery.findByIdAndDelete(req.params.id)
-    res.status(204).json({message: "deletion successful"});
+    const eatery = await Eatery.findByIdAndDelete(req.params.id);
+    res.status(204).json({ message: "deletion successful" });
   } catch (error) {
     res.status(400).send(error.message);
   }
-})
+});
 
-  
-
-
-  
-  
-  module.exports = router;
+module.exports = router;
